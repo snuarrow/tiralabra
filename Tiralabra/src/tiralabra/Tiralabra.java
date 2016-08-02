45dcaa8526220825f565b58fc929f8c99c302cb8
Henri Vaara <henri.vaara@helsinki.fi>
2016-08-02 14:18:09 +0300

    ohjelmarunko viikko1

--- Tiralabra/src/tiralabra/Tiralabra.java
+++ Tiralabra/src/tiralabra/Tiralabra.java
@@ -5,11 +5,6 @@
  */
 package tiralabra;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import static javax.management.Query.gt;
-
 /**
  *
  * @author hexvaara
@@ -22,66 +17,12 @@ public class Tiralabra {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        //ProgramStarter ps = new ProgramStarter();
-        //ps.startFrame();
-        try {
-      ( new TwoWaySerialComm() ).connect( "/dev/ttyUSB0" );
-    } catch( Exception e ) {
-      e.printStackTrace();
-    }
+        ProgramStarter ps = new ProgramStarter();
+        ps.startFrame();
+    
         
         
         
     }
     
 }
-
-class SerialReader implements Runnable {
- 
-    InputStream in;
- 
-    public SerialReader( InputStream in ) {
-      this.in = in;
-    }
- 
-    public void run() {
-      try {
-        while(true ) {
-          System.out.print( in.read() );
-        }
-      } catch( IOException e ) {
-        e.printStackTrace();
-      }
-    }
-  }
-
-
-class TwoWaySerialComm {
- 
-  void connect( String portName ) throws Exception {
-    CommPortIdentifier portIdentifier = CommPortIdentifier
-        .getPortIdentifier( portName );
-    if( portIdentifier.isCurrentlyOwned() ) {
-      System.out.println( "Error: Port is currently in use" );
-    } else {
-      int timeout = 2000;
-      CommPort commPort = portIdentifier.open( this.getClass().getName(), timeout );
- 
-      if( commPort instanceof SerialPort ) {
-        SerialPort serialPort = ( SerialPort )commPort;
-        serialPort.setSerialPortParams( 57600,
-                                        SerialPort.DATABITS_8,
-                                        SerialPort.STOPBITS_1,
-                                        SerialPort.PARITY_NONE );
- 
-        InputStream in = serialPort.getInputStream();
-        OutputStream out = serialPort.getOutputStream();
- 
-        ( new Thread( new SerialReader( in ) ) ).start();
-        ( new Thread( new SerialWriter( out ) ) ).start();
- 
-      } else {
-        System.out.println( "Error: Only serial ports are handled by this example." );
-      }
-    }
-  }
